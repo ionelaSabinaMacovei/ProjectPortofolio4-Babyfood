fa511c9d8950d87d0979a5b49973f473df7d44b5
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2023-04-19 15:34:01 +0000

    Add the basic structure for creating the view for the blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+"""
+Create the view code
+"""
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 6
