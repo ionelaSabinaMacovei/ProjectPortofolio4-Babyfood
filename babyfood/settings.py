0d7f7281f7d48b5d7b9a9a62b720ecb7568fea19
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2023-06-08 09:51:13 +0000

    Update the place of add recipie button on profile page

--- babyfood/settings.py
+++ babyfood/settings.py
@@ -22,7 +22,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/'8000-ionelasabin-projectport-ft3v676wtx2.ws-eu99.gitpod.io
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -32,7 +32,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['project-portofolio4-babyfood.herokuapp.com', 'localhost', '8000-ionelasabin-projectport-ft3v676wtx2.ws-eu98.gitpod.io' ]
+ALLOWED_HOSTS = ['project-portofolio4-babyfood.herokuapp.com', 'localhost', '8000-ionelasabin-projectport-ft3v676wtx2.ws-eu99.gitpod.io' ]
 
 
 # Application definition
