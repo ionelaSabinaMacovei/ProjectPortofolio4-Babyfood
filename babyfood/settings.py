f76f00e597e16a827f14165db0b2b64b7046fa7a
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2023-06-16 14:35:02 +0000

    Add hero section on the about page and gategories page

--- babyfood/settings.py
+++ babyfood/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['project-portofolio4-babyfood.herokuapp.com', 'localhost', '8000-ionelasabin-projectport-ft3v676wtx2.ws-eu99.gitpod.io' ]
+ALLOWED_HOSTS = ['project-portofolio4-babyfood.herokuapp.com', 'localhost', '8000-ionelasabin-projectport-ft3v676wtx2.ws-eu100.gitpod.io'  ]
 
 
 # Application definition
