abae2550652df5b50d9218bfad31f016a1034757
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2023-07-11 09:05:48 +0000

    Set debug to false

--- babyfood/settings.py
+++ babyfood/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['project-portofolio4-babyfood.herokuapp.com', 'localhost', '8000-ionelasabin-projectport-y54vzksfurp.ws-eu101.gitpod.io'  ]
