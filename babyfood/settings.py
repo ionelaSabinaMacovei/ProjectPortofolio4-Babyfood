460e8b446833c394c9bbd6f4a035be0b718b2dc4
ionelaSabinaMacovei <badale.sabina@yahoo.com>
2023-07-12 07:19:35 +0000

    Set debug to False

--- babyfood/settings.py
+++ babyfood/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['project-portofolio4-babyfood.herokuapp.com', 'localhost',
-                 '8000-ionelasabin-projectport-y54vzksfurp.ws-eu101.gitpod.io']
+                 '8000-ionelasabin-projectport-66vgivmedrq.ws-eu101.gitpod.io']
 
 
 # Application definition
